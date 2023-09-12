6ce4ca4cac85c52526f47e00a83c35ebab763d1e
bdehapiot <b.dehapiot@gmail.com>
2023-09-12 17:18:23 +0200

    update

--- predict.py
+++ predict.py
@@ -14,22 +14,21 @@ from skimage.transform import rescale
 #%% Parameters ----------------------------------------------------------------
 
 # Paths
-data_path = Path('D:/local_Meschichi/data')
 data_path = Path(Path.cwd(), 'data', 'local')
-# stack_name = 'KASind1.nd2'
-stack_name = 'KZLind1.nd2'
+stack_name = 'KASind1.nd2'
+# stack_name = 'KZLind1.nd2'
 
 # Training
-rescale_factor = 0.5
+rescale_factor = 0.25
 
 #%% Predict -------------------------------------------------------------------
 
 # Open & format prediction data
-predict_images = nd2.imread(Path(data_path) / stack_name).squeeze()  
-predict_images = rescale(predict_images, (1, rescale_factor, rescale_factor), preserve_range=True)
-pMax = np.percentile(predict_images, 99.9)
-predict_images[predict_images > pMax] = pMax
-predict_images = (predict_images / pMax)
+stack = nd2.imread(Path(data_path) / stack_name).squeeze()  
+stack = rescale(stack, (1, rescale_factor, rescale_factor), preserve_range=True)
+pMax = np.percentile(stack, 99.9)
+stack[stack > pMax] = pMax
+stack = (stack / pMax)
 
 #%% Predict -------------------------------------------------------------------
 
@@ -42,21 +41,29 @@ model = sm.Unet(
     encoder_weights=None,
     )
 model.compile(
-    'Adam', 
+    optimizer='adam',
     loss='binary_crossentropy', 
     metrics=['mse']
     )
 
-# Load the model
-model.load_weights(Path(Path.cwd(), 'model_weights.h5'))
+# Predict masks
+model.load_weights(Path(Path.cwd(), 'model_masks_weights.h5'))
+masks_probs = model.predict(stack).squeeze() 
 
-# Predict
-probs = model.predict(predict_images).squeeze()  
+# Predict centroids
+model.load_weights(Path(Path.cwd(), 'model_centroids_weights.h5'))
+centroids_prob = model.predict(stack).squeeze() 
 
+# Display 
+viewer = napari.Viewer()
+viewer.add_image(stack, scale=[1.3675, 1, 1])
+viewer.add_image(masks_probs, scale=[1.3675, 1, 1])
+# viewer.add_image(centroids_prob, scale=[1.3675, 1, 1])
+ 
 #%%
 
-from skimage.filters import gaussian
-from skimage.measure import label
+# from skimage.filters import gaussian
+# from skimage.measure import label
 
 # sig = 3
 # thresh = 0.5
@@ -67,9 +74,9 @@ from skimage.measure import label
 # test[mask==0] = 0
 
 # Display 
-viewer = napari.Viewer()
-viewer.add_image(predict_images, scale=[2.735, 1, 1])
-viewer.add_image(probs, scale=[2.735, 1, 1])
+# viewer = napari.Viewer()
+# viewer.add_image(predict_images, scale=[2.735, 1, 1])
+# viewer.add_image(probs, scale=[2.735, 1, 1])
 # viewer.add_image(mask, scale=[2.735, 1, 1])
 # viewer.add_labels(labels, scale=[2.735, 1, 1])
 # viewer.add_image(test, scale=[2.735, 1, 1])
--- train_test.py
+++ train_test.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import segmentation_models as sm
 from joblib import Parallel, delayed 
 from skimage.transform import rescale
+from skimage.measure import regionprops
 
 #%% Parameters ----------------------------------------------------------------
 
@@ -22,13 +23,13 @@ data_path = Path(Path.cwd(), 'data')
 train_path = Path(data_path, 'train') 
 
 # Training
-rescale_factor = 0.5
-validation_split = 0.33
-n_epochs = 30
-batch_size = 4
+rescale_factor = 0.25
+validation_split = 0.2
+n_epochs = 100
+batch_size = 8
 
 # Data augmentation
-iterations = 0
+iterations = 500
 augment = True if iterations > 0 else False
 operations = A.Compose([
     A.VerticalFlip(p=0.5),              
@@ -74,80 +75,109 @@ images = (images / pMax).astype(float)
 
 # # Display 
 # viewer = napari.Viewer()
-# viewer.add_image(train_images)
-# viewer.add_image(train_masks)
+# viewer.add_image(images)
+# viewer.add_image(masks)
        
 # -----------------------------------------------------------------------------
 
-# if augment:
+# Get binary object centroids 
+centroids = []
+for label in labels:
+    props = regionprops(label)
+    centroid = np.zeros_like(label)
+    for prop in props:
+        x, y = prop.centroid[1], prop.centroid[0]
+        centroid[round(y), round(x)] = 1
+    centroids.append(centroid)
+centroids = np.stack(centroids).astype(float)
 
-#     # Augment data
-#     def augment_data(train_images, train_masks, operations):      
-#         idx = random.randint(0, len(train_images) - 1)
-#         outputs = operations(image=train_images[idx,...], mask=train_masks[idx,...])
-#         return outputs['image'], outputs['mask']
-#     outputs = Parallel(n_jobs=-1)(
-#         delayed(augment_data)(train_images, train_masks, operations)
-#         for i in range(iterations)
-#         )
-#     train_images = np.stack([data[0] for data in outputs])
-#     train_masks = np.stack([data[1] for data in outputs])
+# # Display 
+# viewer = napari.Viewer()
+# viewer.add_image(masks)
+# viewer.add_image(centroids)  
+
+# -----------------------------------------------------------------------------
+
+if augment:
+
+    # Augment data
+    def augment_data(images, masks, operations):      
+        idx = random.randint(0, len(images) - 1)
+        outputs = operations(image=images[idx,...], mask=centroids[idx,...])
+        return outputs['image'], outputs['mask']
+    outputs = Parallel(n_jobs=-1)(
+        delayed(augment_data)(images, masks, operations)
+        for i in range(iterations)
+        )
+    images = np.stack([data[0] for data in outputs])
+    masks = np.stack([data[1] for data in outputs])
     
     # # Display 
     # viewer = napari.Viewer()
     # viewer.add_image(train_images)
     # viewer.add_labels(train_masks)     
 
-# -----------------------------------------------------------------------------
-
-# from skimage.measure import regionprops
-
-# train_centroids = []
-# for mask in train_masks:
-#     props = regionprops(mask)
-#     centroid = np.zeros_like(mask)
-#     for ctrd in props.centroid:
-#         centroid[ctrd] = 1
-#     train_centroids.append(centroid)
-
 #%% Train model ---------------------------------------------------------------
 
-# # Define & compile model
-# model = sm.Unet(
-#     'resnet34', 
-#     input_shape=(None, None, 1), 
-#     classes=1, 
-#     activation='sigmoid', 
-#     encoder_weights=None,
-#     )
-# model.compile(
-#     'Adam', 
-#     loss='binary_crossentropy', 
-#     metrics=['mse']
-#     )
+# Define & compile model
+model = sm.Unet(
+    'resnet34', 
+    input_shape=(None, None, 1), 
+    classes=1, 
+    activation='sigmoid', 
+    encoder_weights=None,
+    )
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy', 
+    metrics=['mse']
+    )
 
-# # Train model
-# callbacks = [tf.keras.callbacks.EarlyStopping(patience=10, monitor='val_loss')]
-# history = model.fit(
-#     x=train_images,
-#     y=train_masks,
-#     validation_split=validation_split,
-#     batch_size=batch_size,
-#     epochs=n_epochs,
-#     callbacks=callbacks,
-# )
+# Train model
+callbacks = [tf.keras.callbacks.EarlyStopping(patience=20, monitor='val_loss')]
+history = model.fit(
+    x=images,
+    y=masks,
+    validation_split=validation_split,
+    batch_size=batch_size,
+    epochs=n_epochs,
+    callbacks=callbacks,
+)
 
-# # Plot training results
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-# epochs = range(1, len(loss) + 1)
-# plt.plot(epochs, loss, 'y', label='Training loss')
-# plt.plot(epochs, val_loss, 'r', label='Validation loss')
-# plt.title('Training and validation loss')
-# plt.xlabel('Epochs')
-# plt.ylabel('Loss')
-# plt.legend()
-# plt.show()
+# Plot training results
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
 
-# # # Save model
-# model.save_weights(Path(Path.cwd(), 'model_weights.h5'))
+# Save model
+# model.save_weights(Path(Path.cwd(), 'model_masks_weights.h5'))
+model.save_weights(Path(Path.cwd(), 'model_centroids_weights.h5'))
+
+#%% Predict -------------------------------------------------------------------
+
+# Paths
+data_path = Path(Path.cwd(), 'data', 'local')
+stack_name = 'KASind1.nd2'
+stack_name = 'KZLind1.nd2'
+
+# Open & format prediction data
+stack = nd2.imread(Path(data_path) / stack_name).squeeze()  
+stack = rescale(stack, (1, rescale_factor, rescale_factor), preserve_range=True)
+pMax = np.percentile(stack, 99.9)
+stack[stack > pMax] = pMax
+stack = (stack / pMax)
+
+# Predict
+probs = model.predict(stack).squeeze()  
+
+# Display 
+viewer = napari.Viewer()
+viewer.add_image(stack)
+viewer.add_image(probs)   
