8d23fb1d2fc407e169f927c3691acfcefc86daa3
bdehapiot <b.dehapiot@gmail.com>
2023-09-14 18:55:41 +0200

    update

--- predict.py
+++ predict.py
@@ -11,8 +11,8 @@ from skimage.transform import rescale
 
 # Get stack name
 # stack_name = 'KASind1.nd2'
-stack_name = 'KASind1001.nd2'
-# stack_name = 'KZLind1.nd2'
+# stack_name = 'KASind1001.nd2'
+stack_name = 'KZLind1.nd2'
 # stack_name = 'KALind1001.nd2'
 
 # Get paths
@@ -30,6 +30,7 @@ rescale_factor = float(model_name[14:17])
 # Open data to predict (stack)
 with nd2.ND2File(Path(data_path) / stack_name) as ndfile:
     stack = ndfile.asarray()
+    stack_raw = stack.copy()
     voxel_size = list(ndfile.voxel_size())
     z_ratio = (voxel_size[2] / voxel_size[0]) * rescale_factor
     
@@ -70,7 +71,7 @@ from skimage.filters import gaussian
 from skimage.measure import label
 
 # Parameters
-sig = 1
+sig = 2
 thresh = 0.5
 
 # Post-processing
@@ -78,10 +79,43 @@ probs = gaussian(probs, sigma=(sig / z_ratio, sig, sig))
 mask = probs > thresh
 labels = label(mask)
 
+#%% Watershed -----------------------------------------------------------------
+
+# from scipy.ndimage import distance_transform_edt
+# from skimage.feature import peak_local_max
+# from skimage.segmentation import watershed
+
+# # Distance transform
+# distance = distance_transform_edt(mask, sampling=(z_ratio, 1, 1))
+
+# # Local maxima
+# local_max = peak_local_max(distance, min_distance=12, exclude_border=False)
+
+# # # Display 
+# viewer = napari.Viewer()
+# viewer.add_image(distance, scale=[z_ratio, 1, 1])
+# viewer.add_points(local_max, scale=[z_ratio, 1, 1])
+
+#%%
+
+# from skimage.restoration import rolling_ball
+from skimage.morphology import white_tophat, disk
+
+tophat = []
+for plane in stack:
+    tophat.append(white_tophat(plane, footprint=disk(5)))
+tophat = np.stack(tophat)
+
 # Display 
 viewer = napari.Viewer()
 viewer.add_image(stack, scale=[z_ratio, 1, 1])
-viewer.add_image(probs, scale=[z_ratio, 1, 1])
-viewer.add_image(mask, scale=[z_ratio, 1, 1])
-viewer.add_labels(labels, scale=[z_ratio, 1, 1])
-# viewer.add_image(test, scale=[z_ratio, 1, 1])
+viewer.add_image(tophat, scale=[z_ratio, 1, 1])
+
+#%%
+
+# # Display 
+# viewer = napari.Viewer()
+# viewer.add_image(stack, scale=[z_ratio, 1, 1])
+# viewer.add_image(probs, scale=[z_ratio, 1, 1])
+# viewer.add_image(mask, scale=[z_ratio, 1, 1])
+# viewer.add_labels(labels, scale=[z_ratio, 1, 1])
