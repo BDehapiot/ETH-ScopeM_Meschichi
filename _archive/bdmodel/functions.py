d13b805eb17bfb618938e744a96e3b6c70190fae
bdehapiot <b.dehapiot@gmail.com>
2025-02-24 10:02:10 +0100

    update

--- _archive/bdmodel/functions.py
+++ _archive/bdmodel/functions.py
@@ -13,6 +13,8 @@ from bdtools.norm import norm_gcn, norm_pct
 from bdtools.patch import extract_patches
 
 # Skimage
+from skimage.filters import gaussian
+from skimage.exposure import adjust_gamma
 from skimage.segmentation import find_boundaries 
 
 #%% Function: get_paths() -----------------------------------------------------
@@ -256,34 +258,193 @@ def preprocess(
     
 #%% Function: augment() -------------------------------------------------------
 
-def augment(imgs, msks, iterations):
+# def augment(imgs, msks, iterations):
         
-    if iterations <= imgs.shape[0]:
-        warnings.warn(f"iterations ({iterations}) is less than n of images")
+#     if iterations <= imgs.shape[0]:
+#         warnings.warn(f"iterations ({iterations}) is less than n of images")
         
-    # Nested function(s) ------------------------------------------------------
+#     # Nested function(s) ------------------------------------------------------
     
-    def _augment(imgs, msks):     
+#     def _augment(imgs, msks):     
         
-        operations = A.Compose([
-            A.VerticalFlip(p=0.5),              
-            A.RandomRotate90(p=0.5),
-            A.HorizontalFlip(p=0.5),
-            A.Transpose(p=0.5),
-            A.GridDistortion(p=0.5),
-            ])
+#         operations = A.Compose([
+#             A.VerticalFlip(p=0.5),              
+#             A.RandomRotate90(p=0.5),
+#             A.HorizontalFlip(p=0.5),
+#             A.Transpose(p=0.5),
+#             A.GridDistortion(p=0.5),
+#             ])
         
-        idx = np.random.randint(0, len(imgs) - 1)
-        outputs = operations(image=imgs[idx,...], mask=msks[idx,...])
-        return outputs["image"], outputs["mask"]
+#         idx = np.random.randint(0, len(imgs) - 1)
+#         outputs = operations(image=imgs[idx,...], mask=msks[idx,...])
+#         return outputs["image"], outputs["mask"]
     
+#     # Execute -----------------------------------------------------------------
+    
+#     outputs = Parallel(n_jobs=-1)(
+#         delayed(_augment)(imgs, msks)
+#         for i in range(iterations)
+#         )
+#     imgs = np.stack([data[0] for data in outputs])
+#     msks = np.stack([data[1] for data in outputs])
+    
+#     return imgs, msks
+
+def augment(
+        imgs, msks, iterations,
+        gamma_p=0.5, gblur_p=0.5, noise_p=0.5, flip_p=0.5, distord_p=0.5,
+        ):
+    
+    """
+    Augment images and masks using random transformations.
+    
+    The following transformation are applied:
+        
+        - adjust gamma (image only)      
+        - apply gaussian blur (image only) 
+        - add noise (image only) 
+        - flip (image & mask)
+        - grid distord (image & mask)
+    
+    If required, image transformations are applied to their correponding masks.
+    Transformation probabilities can be set with function arguments.
+    Transformation random parameters can be tuned with the params dictionnary.
+    Grid distortions are applied with the `albumentations` library.
+    https://albumentations.ai/
+
+    Parameters
+    ----------
+    imgs : 3D ndarray (float)
+        Input image(s).
+        
+    msks : 3D ndarray (float) 
+        Input corresponding mask(s).
+        
+    iterations : int
+        The number of augmented samples to generate.
+    
+    gamma_p, gblur_p, noise_p, flip_p, distord_p : float (0 to 1) 
+        Probability to apply the transformation.
+    
+    Returns
+    -------
+    imgs : 3D ndarray (float)
+        Augmented image(s).
+        
+    msks : 3D ndarray (float) 
+        Augmented corresponding mask(s).
+    
+    """
+    
+    # Parameters --------------------------------------------------------------
+    
+    params = {
+        
+        # Gamma
+        "gamma_low"  : 0.75,
+        "gamma_high" : 1.25,
+        
+        # Gaussian blur
+        "sigma_low"  : 1,
+        "sigma_high" : 3,
+        
+        # Noise
+        "sgain_low"   : 20,
+        "sgain_high"  : 50,
+        "rnoise_low"  : 2,
+        "rnoise_high" : 4,
+        
+        # Grid distord
+        "nsteps_low"  : 1,
+        "nsteps_high" : 10,
+        "dlimit_low"  : 0.1,
+        "dlimit_high" : 0.5,
+        
+        }
+    
+    # Nested functions --------------------------------------------------------
+    
+    def _gamma(img, gamma=1.0):
+        img_mean = np.mean(img)
+        img = adjust_gamma(img, gamma=gamma)
+        img = img * (img_mean / np.mean(img))
+        return img
+    
+    def _noise(img, shot_gain=0.1, read_noise_std=5):
+        img_std = np.std(img) 
+        # img = np.random.poisson(img * shot_gain) / shot_gain
+        img += np.random.normal(
+            loc=0.0, scale=img_std / read_noise_std, size=img.shape)
+        return img
+    
+    def _flip(img, msk):
+        if np.random.rand() < 0.5:
+            img, msk = np.flipud(img), np.flipud(msk)
+        if np.random.rand() < 0.5:
+            img, msk = np.fliplr(img), np.fliplr(msk)
+        if img.shape[0] == img.shape[1]:
+            if np.random.rand() < 0.5:
+                k = np.random.choice([-1, 1])
+                img = np.rot90(img, k=k)
+                msk = np.rot90(msk, k=k)
+        return img, msk
+    
+    def _augment(img, msk):
+        
+        img = img.copy()
+        msk = msk.copy()
+        
+        if np.random.rand() < gamma_p:
+            gamma = np.random.uniform(
+                params["gamma_low"], params["gamma_high"])
+            img = _gamma(img, gamma=gamma)
+            
+        if np.random.rand() < gblur_p:
+            sigma = np.random.randint(
+                params["sigma_low"], params["sigma_high"])
+            img = gaussian(img, sigma=sigma)
+            
+        if np.random.rand() < noise_p:
+            shot_gain = np.random.uniform(
+                params["sgain_low"], params["sgain_high"])
+            read_noise_std = np.random.randint(
+                params["rnoise_low"], params["rnoise_high"])
+            img = _noise(
+                img, shot_gain=shot_gain, read_noise_std=read_noise_std)
+            
+        if np.random.rand() < flip_p:
+            img, msk = _flip(img, msk)
+            
+        if np.random.rand() < distord_p:
+            num_steps = np.random.randint(
+                params["nsteps_low"], params["nsteps_high"])
+            distort_limit = np.random.uniform(
+                params["dlimit_low"], params["dlimit_high"])
+            spatial_transforms = A.Compose([
+                A.GridDistortion(
+                    num_steps=num_steps, 
+                    distort_limit=distort_limit, 
+                    p=1
+                    )
+                ])
+            outputs = spatial_transforms(image=img, mask=msk)
+            img, msk = outputs["image"], outputs["mask"]
+        
+        return img, msk
+        
     # Execute -----------------------------------------------------------------
     
-    outputs = Parallel(n_jobs=-1)(
-        delayed(_augment)(imgs, msks)
-        for i in range(iterations)
+    # Initialize
+    imgs = imgs.astype("float32")
+    idxs = np.random.choice(
+        np.arange(0, imgs.shape[0]), size=iterations)
+    
+    outputs = Parallel(n_jobs=-1, backend="threading")(
+        delayed(_augment)(imgs[i], msks[i])
+        for i in idxs
         )
+    
     imgs = np.stack([data[0] for data in outputs])
     msks = np.stack([data[1] for data in outputs])
-    
+        
     return imgs, msks
--- train.py
+++ train.py
@@ -1,49 +1,57 @@
 #%% Imports -------------------------------------------------------------------
 
+import napari
+import numpy as np
 from skimage import io
 from pathlib import Path
 
-# bdmodel
-from bdmodel.train import Train
-from bdmodel.functions import get_paths
+# bdtools
+from bdtools.models import preprocess
+
+#%% Inputs --------------------------------------------------------------------
+
+target = "rscale" 
+patch_size = 128
+patch_overlap = 0
+
+#%% Initialize ----------------------------------------------------------------
+
+train_path = Path("data", "train")
 
 #%% Execute -------------------------------------------------------------------
 
 if __name__ == "__main__":
-
-    # Paths
-    train_path = Path("data", "train")
     
-    # Open data
-    imgs, msks = [], []
-    msk_paths = get_paths(
-        train_path, 
-        ext=".tif", 
-        subfolders=False,
-        )
-    for path in msk_paths:
-        imgs.append(io.imread(str(path).replace("_mask", "")))
-        msks.append(io.imread(path))
-        
-    # Train
-    train = Train(
-        imgs, msks,
-        save_name="rslice_image_128",
-        save_path=Path("bdmodel"),
-        msk_type="normal",
-        img_norm="image",
-        patch_size=128,
-        patch_overlap=0,
-        nAugment=2000,
-        backbone="resnet18",
-        epochs=200,
-        batch_size=32,
-        validation_split=0.2,
-        learning_rate=0.0005,
-        patience=30,
-        weights_path="",
-        # weights_path=Path(Path.cwd(), "rscale_128", "weights.h5"),
+    # Load data
+    X, y = [], []
+    for path in list(train_path.glob(f"*{target}*")):
+        if not "mask" in path.name:
+            X.append(io.imread(path))
+        else:
+            y.append(io.imread(path))
+    X = np.stack(X)
+    y = np.stack(y)
+    
+    # Remove empty masks
+    valid = []
+    for i in range(len(X)):
+        if np.max(y[i, ...]) > 0:
+            valid.append(i)
+    X = X[valid]
+    y = y[valid]
+          
+    # Preprocess
+    X, y = preprocess(
+        X, msks=y, 
+        patch_size=patch_size, 
+        patch_overlap=patch_overlap,
+        img_norm="global",
+        msk_type="edt",
         )
     
-    test = Path("bdmodel")
-    print(test.resolve())
+    # Display
+    viewer = napari.Viewer()
+    viewer.add_image(X)
+    viewer.add_image(y)
+    
+    pass
