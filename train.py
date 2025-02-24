ef3681ca27baa0508b233d5dff1e2676ce18f7ec
bdehapiot <b.dehapiot@gmail.com>
2025-02-24 11:25:05 +0100

    update

--- train.py
+++ train.py
@@ -6,52 +6,92 @@ from skimage import io
 from pathlib import Path
 
 # bdtools
-from bdtools.models import preprocess
+from bdtools.models.metrics import *
+from bdtools.models import preprocess, train
+
 
 #%% Inputs --------------------------------------------------------------------
 
+# General
 target = "rscale" 
+
+# preprocess()
 patch_size = 128
 patch_overlap = 0
+img_norm = "global"
+msk_type = "edt"
+
+# UNet build()
+save_name = f"{target}"
+backbone = "resnet18"
+activation = "sigmoid"
+downscale_steps = 1
+
+# UNet train()
+epochs = 100
+batch_size = 32 
+validation_split = 0.2
+metric = "soft_dice_coef"
+learning_rate = 0.0005
+patience = 20
 
 #%% Initialize ----------------------------------------------------------------
 
 train_path = Path("data", "train")
 
-#%% Execute -------------------------------------------------------------------
+#%% Eimgsecute -------------------------------------------------------------------
 
 if __name__ == "__main__":
     
     # Load data
-    X, y = [], []
+    imgs, msks = [], []
     for path in list(train_path.glob(f"*{target}*")):
         if not "mask" in path.name:
-            X.append(io.imread(path))
+            imgs.append(io.imread(path))
         else:
-            y.append(io.imread(path))
-    X = np.stack(X)
-    y = np.stack(y)
+            msks.append(io.imread(path))
+    imgs = np.stack(imgs)
+    msks = np.stack(msks)
     
     # Remove empty masks
     valid = []
-    for i in range(len(X)):
-        if np.max(y[i, ...]) > 0:
+    for i in range(len(imgs)):
+        if np.max(msks[i, ...]) > 0:
             valid.append(i)
-    X = X[valid]
-    y = y[valid]
+    imgs = imgs[valid]
+    msks = msks[valid]
           
     # Preprocess
-    X, y = preprocess(
-        X, msks=y, 
+    imgs, msks = preprocess(
+        imgs, msks=msks, 
         patch_size=patch_size, 
         patch_overlap=patch_overlap,
-        img_norm="global",
-        msk_type="edt",
+        img_norm=img_norm,
+        msk_type=msk_type,
+        )
+    
+    # Train
+    unet = UNet(
+        save_name=save_name,
+        backbone=backbone,
+        activation=activation,
+        downscale_steps=downscale_steps, 
+        )
+
+    unet.train(
+        imgs, msks, 
+        X_val=None, y_val=None,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_split=validation_split,
+        metric=metric,
+        learning_rate=learning_rate,
+        patience=patience,
         )
     
     # Display
     viewer = napari.Viewer()
-    viewer.add_image(X)
-    viewer.add_image(y)
+    viewer.add_image(imgs)
+    viewer.add_image(msks)
     
     pass
